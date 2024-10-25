6a72971a8a1dbe1f794cb018b9934d8fe65a23d2
DhanashriSolanki <dshri1807@gmail.com>
2024-10-25 17:07:04 +0530

    Folders added

---
+++ template.py
@@ -0,0 +1,48 @@
+import os
+from pathlib import Path  # used for managing paths and slashes
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]:%(message)s:')
+
+project_name = "textSummariser"
+
+# useful for ci/cd deployment
+# file and folders creation
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/logging/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "app.py",
+    "main.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb"
+]
+
+# logic for implementation of these files
+for filepath in list_of_files:
+    # detects the os of this system and returns the path
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Created directory: {filedir} for the file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Created empty file: {filepath}")
+
+    else:
+        logging.info(f"File {filepath} already exists")
